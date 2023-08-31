ed69cd0f73aaecbd303a4218964c235e6afad011
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2023-08-30 21:06:11 -0300

    Add files via upload

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,3 +1,4 @@
+/* Scrol Header */
 window.onscroll=function(){
     rolagem()
 }
@@ -11,4 +12,51 @@ function rolagem(){
         document.getElementById("logo").style.fontSize="20px"
         document.getElementById("logo").style.width="30%"
     }
+}
+
+/* Carousel Time */
+
+const controls = document.querySelectorAll(".control");
+let currentItem = 0;
+const items = document.querySelectorAll(".item");
+const maxItems = items.length;
+
+controls.forEach((control) => {
+  control.addEventListener("click", (e) => {
+    isLeft = e.target.classList.contains("arrow-left");
+
+    if (isLeft) {
+      currentItem -= 1;
+    } else {
+      currentItem += 1;
+    }
+
+    if (currentItem >= maxItems) {
+      currentItem = 0;
+    }
+    if (currentItem < 0) {
+      currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].classList.add("current-item");
+  });
+});
+
+setInterval(timer, 7500);
+
+function timer(){
+    currentItem += 1
+
+    if (currentItem >= maxItems) {
+        currentItem = 0;
+      }
+    if (currentItem < 0) {
+        currentItem = maxItems - 1;
+    }
+
+    items.forEach((item) => item.classList.remove("current-item"));
+
+    items[currentItem].classList.add("current-item");
 }
