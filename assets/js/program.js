6d19d52fdb7cbe07d531e134178011c5fd01ca61
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2023-10-28 18:41:25 -0300

    Add files via upload

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,27 +1,30 @@
 /* Scrol Header */
-
 window.onscroll=function(){
     rolagem()
 }
+
 function rolagem(){
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
         document.getElementsByTagName("header")[0].style.padding="1px 1px"
         document.getElementById("logo").style.fontSize="1px"
         document.getElementById("logo").style.width="20%"
         document.getElementById("header").style.backgroundColor="var(--VerdeEscuro-cor)"
+        document.getElementById("nav-bg").style.top="50px"
+        document.getElementById("nav-bg").style.backgroundColor="var(--VerdeEscuro-cor)"
         
     }else{
         document.getElementsByTagName("header")[0].style.padding="20px 10px"
         document.getElementById("logo").style.fontSize="20px"
         document.getElementById("logo").style.width="30%"
         document.getElementById("header").style.backgroundColor="var(--Verde-cor)"
+        document.getElementById("nav-bg").style.top="79px"
+        document.getElementById("nav-bg").style.backgroundColor="var(--Verde-cor)"
     }
 }
 
 
 
 /* Carousel Time */
-
 const controls = document.querySelectorAll(".control");
 let currentItem = 0;
 const items = document.querySelectorAll(".item");
@@ -69,7 +72,6 @@ function timer(){
 
 
 /* Animação */
-
 const target = document.querySelectorAll('[data-anime]');
 const AnimationClass = 'animate';
 
@@ -85,6 +87,7 @@ function animeScroll(){
     console.log(_element.offsetTop);
 
   })
+
 }
 
 animeScroll();
@@ -93,3 +96,38 @@ window.addEventListener('scroll', function(){
   animeScroll();
 });
 
+
+
+/* Menu active */
+
+window.addEventListener("scroll", navHighlighter);
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+function navHighlighter(){
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      });
+    };
+  });
+};
+
+
+
+/* Menu responsivo */
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.menu');
+const navbg = document.querySelector('.nav-bg');
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
